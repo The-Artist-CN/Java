517b9867d6c058cf5af8ea1b31461b072071cfcc
Jerry <Arleneyrd987@gmail.com>
2025-01-03 14:53:56 +0800

    Jan 3 14:55PM

--- String/Prac/example.java
+++ String/Prac/example.java
@@ -1,19 +1,17 @@
 public class example {
-    public static void main(String[] args){
-        // String greeting = "Hello";
-        // String trim = greeting.substring(0,3);
-        // System.out.println(trim);
-        // String aa = new String("aa");
-        // String bb = "aa";
-        // String cc = "aa";
-        // System.out.println(bb == cc);
-        // System.out.println(aa == bb);
-        // System.out.println(aa.equals(cc));
-        // System.out.println(aa.equals(bb));
+    public static void main(String[] args) {
+        String greeting = "Hello";
+        String trim = greeting.substring(0, 3);
+        String aa = new String("aa");
+        String bb = "aa";
+        String cc = "aa";
+        String hh = "hello_world";
+        System.out.println(bb == cc);
+        System.out.println(aa == bb);
+        System.out.println(aa.equals(cc));
+        System.out.println(aa.equals(bb));
+        System.out.println(trim);
 
-        // String hh = "hello_world";
-        // System.out.println(hh.substring(0,5).intern() == "hello");
-        // System.out.println(hh.substring(0, 5).equals("hello"));
-        
+
     }
 }
