7ead7b2be75e72264ac76dd845a8e34b67f83602
Jerry <Arleneyrd987@gmail.com>
2025-01-03 13:43:15 +0800

    Jan 3 13:45PM

---
+++ String/Prac/example.java
@@ -0,0 +1,19 @@
+public class example {
+    public static void main(String[] args){
+        // String greeting = "Hello";
+        // String trim = greeting.substring(0,3);
+        // System.out.println(trim);
+        // String aa = new String("aa");
+        // String bb = "aa";
+        // String cc = "aa";
+        // System.out.println(bb == cc);
+        // System.out.println(aa == bb);
+        // System.out.println(aa.equals(cc));
+        // System.out.println(aa.equals(bb));
+
+        // String hh = "hello_world";
+        // System.out.println(hh.substring(0,5).intern() == "hello");
+        // System.out.println(hh.substring(0, 5).equals("hello"));
+        
+    }
+}
