4c11d1430ba914cff504b190c14c7eecd097d2d6
Tom <chqteng1234@163.com>
2025-01-04 03:30:29 +0800

    Jan 4 3：20PM

--- String/Prac/String_Test.java
+++ String/Prac/String_Test.java
@@ -7,11 +7,17 @@ public class String_Test {
         String cc = "aa";
         String hh = "hello_world";
         System.out.println(bb == cc);
+        String aa = "aa";
+        String bb = new String("aa");
+        String cc = bb;
+        String dd = "aa";
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        System.out.println(c);
         System.out.println(aa == bb);
-        System.out.println(aa.equals(cc));
-        System.out.println(aa.equals(bb));
-        System.out.println(trim);
-
-
+        System.out.println(aa == dd);
+        System.out.println(bb.intern() == aa);
+        System.out.println(cc == bb);
     }
 }
